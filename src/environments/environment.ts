cc0019467eb25ce6f77831c02c9b64dad60a15fe
alvarosr89 <alvaro.santacruz@grupodiusframi.com.co>
2024-09-20 16:18:38 -0500

    front terminado ajustes finales cambio https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://back-tomas-production.up.railway.app/api/'
+  url: 'https://back-tomas-production.up.railway.app/api/'
   //url: 'http://localhost:8080/api/'
 };
 
